dd965debecbdfc1398b040c470975cb9309237c5
Amanda Silveira - Br Supply <amanda.silveira@brsupply.com.br>
2024-05-11 22:05:36 -0300

    Adicionando código do projeto React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { useEffect } from "react";
+
+function App() {
+  return (
+    //Global State
+
+    //Navegation
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
